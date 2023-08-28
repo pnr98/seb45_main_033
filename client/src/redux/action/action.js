b2ec289f5d3632ddf4008c67ec26ddb3320e0a01
YunSunMoon <ysm990926@gmail.com>
2023-08-28 11:15:39 +0900

    feat/초기세팅

---
+++ client/src/redux/action/action.js
@@ -0,0 +1,12 @@
+export const setLoginStatus = (status) => {
+    return {
+        type: 'SET_LOGIN_STATUS',
+        payload: status,
+    }  
+  };
+export const setMousePosition = ({x,y}) =>{
+    return {
+       type: 'SET_MOUSE_POSITION',
+       payload: { x: x, y: y }
+    }
+}
