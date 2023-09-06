98df511cf1fff61e915a633383293fc3903d35ce
haaansaaaem <haaansaaaem@gmail.com>
2023-09-07 01:51:34 +0900

    Refactor: 레시피 더보기 페이지 Styled 수정

--- client/src/pages/RecipeSearchPage/RecipeSearchPage.Styled.js
+++ client/src/pages/RecipeSearchPage/RecipeSearchPage.Styled.js
@@ -1,19 +1,44 @@
-import {styled} from 'styled-components';
+import { styled } from 'styled-components';
+import searchIcon1 from '../../common/image/search1.png';
 
 export const SearchContainer = styled.div`
-    max-width: var(--main-width);
-    margin: 0 auto;
-    margin-top: 100px;
-    padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3vw;
+`;
+
+export const SearchWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid var(--grey);
+    border-radius: 5px;
+    margin-bottom: 50px;
+    padding: 5px;
+    width: 45vw;
+`;
+
+export const SearchIcon = styled.div`
+    background-image: url(${searchIcon1});
+    width: 30px;
+    height: 30px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: 0 10px;
 `;
 
 export const SearchInput = styled.input`
-    width: 100%;
+    flex: 1;
     padding: 10px 15px;
     font-size: var(--font-size-16);
-    border: 1px solid var(--grey);
-    border-radius: 5px;
-    margin-bottom: 20px;
+    border: none;
+    outline: none;
+`;
+
+export const Hr = styled.hr`
+  width: var(--main-width);
+  color: #AAAAAA;
+  margin-bottom: 80px;
 `;
 
 export const RecipesContainer = styled.div`
@@ -21,10 +46,17 @@ export const RecipesContainer = styled.div`
     grid-template-columns: repeat(5, 1fr);
     gap: 20px;
     margin-top: 20px;
+    width: var(--main-width);
+
+  @media (max-width: 1300px) {
+    /**/
+  };
 `;
 
 export const PaginationContainer = styled.div`
-    margin-top: 20px;
+    margin: 80px 0 70px 0;
     text-align: center;
+
+    border: 1px solid red;
 `;
 
--- client/src/pages/RecipeSearchPage/RecipeSearchPage.js
+++ client/src/pages/RecipeSearchPage/RecipeSearchPage.js
@@ -1,33 +1,83 @@
+import React from 'react';
 import Recipe from '../../components/Recipe/Recipe';
-import { SearchContainer, SearchInput, RecipesContainer , PaginationContainer} from "./RecipeSearchPage.Styled"
+import { SearchContainer, SearchWrapper, SearchIcon, SearchInput, Hr, RecipesContainer, PaginationContainer } from './RecipeSearchPage.Styled'; // Make sure to import the updated styled components
 
 const RecipeSearchPage = () => {
+  const dummy = [
+    {
+      recipeId: 1,
+      thumbnailUrl: 'https://source.unsplash.com/random/?food',
+      recipeName: '임시 레시피 1',
+      foodType: '한식',
+      difficulty: '하',
+      cook_time: '30분',
+      nutritions: null,
+    },
+    {
+      recipeId: 2,
+      thumbnailUrl: 'https://source.unsplash.com/random/?meal',
+      recipeName: '임시 레시피 2',
+      foodType: '일식',
+      difficulty: '중',
+      cook_time: '45분',
+      nutritions: null,
+    },
+    {
+      recipeId: 3,
+      thumbnailUrl: 'https://source.unsplash.com/random/?cook',
+      recipeName: '임시 레시피 3',
+      foodType: '양식',
+      difficulty: '상',
+      cook_time: '60분',
+      nutritions: null,
+    },
+    {
+      recipeId: 4,
+      thumbnailUrl: 'https://source.unsplash.com/random/?food',
+      recipeName: '임시 레시피 4',
+      foodType: '한식',
+      difficulty: '하',
+      cook_time: '30분',
+      nutritions: null,
+    },
+    {
+      recipeId: 5,
+      thumbnailUrl: 'https://source.unsplash.com/random/?meal',
+      recipeName: '임시 레시피 5',
+      foodType: '일식',
+      difficulty: '중',
+      cook_time: '45분',
+      nutritions: null,
+    },
+  ];
 
-  const dummy = {
-    recipeId: 1,
-    thumbnailUrl: 'https://example.com/image.jpg',
-    recipeName: '임시 레시피',
-    foodType: '한식',
-    difficulty: '하',
-    cook_time: '30분',
-    nutritions: null
-};
+  const dummyRecipes = Array(5).fill(dummy);
 
-    return (
-          <SearchContainer>
-              <SearchInput type="text" placeholder="레시피 검색" />
-            <RecipesContainer>
-              {Array(25).fill().map((_, idx) => (
-                <Recipe key={idx} info={dummy} />
-              ))}
-            </RecipesContainer>
+  return (
+    <SearchContainer>
+      
+      <SearchWrapper>
+        <SearchIcon alt="Search Icon" /> 
+        <SearchInput type="text" placeholder="search..." />
+      </SearchWrapper>
 
-            <PaginationContainer>
-                {/* 페이지네이션 컴포넌트 넣기 */}
-            </PaginationContainer>
-          </SearchContainer>
+    <Hr />
 
-    );
+    <RecipesContainer>
+      {dummyRecipes.flatMap((data, idx) => (
+        <div key={idx}>
+          {data.map((recipeData, recipeIdx) => (
+            <Recipe key={recipeIdx} info={recipeData} />
+        ))}
+        </div>
+      ))}
+    </RecipesContainer>
+
+    <PaginationContainer>
+      페이지네이션 컴포넌트 자리
+    </PaginationContainer>
+  </SearchContainer>
+  );
 };
 
 export default RecipeSearchPage;
