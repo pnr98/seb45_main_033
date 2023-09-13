345a3bbe7971d6534172b8544285de73e3e12222
kimjekyeong <jekyeong1028@gmail.com>
2023-09-13 14:18:00 +0900

    Feat: 좋아요 기능 구현

---
+++ server/src/main/java/com/main33/server/recipe/recipe_like/service/RecipeLikeService.java
@@ -0,0 +1,33 @@
+package com.main33.server.recipe.recipe_like.service;
+
+import com.main33.server.member.entity.Member;
+import com.main33.server.recipe.recipe.entity.Recipe;
+import com.main33.server.recipe.recipe_like.entity.RecipeLike;
+import com.main33.server.recipe.recipe_like.repository.RecipeLikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RecipeLikeService {
+    private final RecipeService recipeService;
+    private final RecipeRepository recipeRepository;
+    private final RecipeLikeRepository recipeLikeRepository;
+
+    public void addLike(Long recipeId, Member member) {
+
+        Recipe recipe = recipeService.findByRecipeId(recipeId);
+
+        if(!recipeLikeRepository.existsByMemberAndRecipe(member, recipe)) {
+            // 호출되면 recipe에 있는 count 증가
+            recipe.setLikeCount(recipe.getLikeCount() + 1);
+            // RecipelikeRepository에 memberId, recipeId값 저장
+            recipeLikeRepository.save(new RecipeLike(member, recipe));
+        } else { // 한번 더 누르면 recipe에 있는 count 감소(취소)
+            recipe.setLikeCount(recipe.getLikeCount() -1);
+            recipeLikeRepository.deleteByMemberAndRecipe(member, recipe);
+        }
+    }
+}
