438d71e12165440ae78789aacb518667a6a96334
spirit998 <spirit998@naver.com>
2023-09-18 09:44:39 +0900

    Feat: Recipe CRUD 기능 및 Recipe 클래스들 기능 구현

--- server/src/main/java/com/main33/server/recipe/recipe/dto/RecipeDto.java
+++ server/src/main/java/com/main33/server/recipe/recipe/dto/RecipeDto.java
@@ -124,10 +124,67 @@ public class RecipeDto {
 
     // For DELETE /recipes/{recipe-id} (DTO에 추가할 필요가 없음)
 
+    // For /
     @Getter
-    @Setter
-    public static class Response {
+    public static class RecipePostResponse {
         private String message;
         private Long recipeId;
+
+        public RecipePostResponse(String message) {
+            this.message = message;
+        }
+
+        public RecipePostResponse(String message, Long recipeId) {
+            this.message = message;
+            this.recipeId = recipeId;
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class RecipeDetailResponse {
+        @NotNull
+        private FoodType foodType;
+
+        private String difficulty;
+
+        @NotBlank
+        private String recipeName;
+
+        private String mainImageUrl;
+
+        private String recipeDescription;
+
+        @NotBlank
+        private String userName;
+
+        private Integer cookingTime;
+
+        private Long views;
+
+        private Long likes;
+
+        private Long commentCount;
+
+        @NotNull
+        private Timestamp timestamp;
+
+        @NotEmpty
+        @Valid
+        private List<Step> steps;
+
+        @NotEmpty
+        private List<String> ingredients;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public class RelatedRecipeDto {
+        private Long recipeId;
+        private String recipeName;
+        private String mainImageUrl;
+        private String foodType;
+        private String difficulty;
+        private Integer cookTime;
     }
 }
--- server/src/main/java/com/main33/server/recipe/recipe/service/RecipeService.java
+++ server/src/main/java/com/main33/server/recipe/recipe/service/RecipeService.java
@@ -11,10 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Principal;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
@@ -27,38 +31,78 @@ public class RecipeService {
         this.memberRepository = memberRepository;
     }
 
-    public Recipe createRecipe(Recipe recipe, Principal principal) {
-        if (principal == null) {
-            throw new BusinessLogicException(ExceptionCode.UNAUTHORIZED_ACCESS);
-        }
-
-        // Principal에서 사용자 이름을 가져와서, 이를 통해 사용자의 고유 ID를 찾음
-        String username = principal.getName();
+    @Transactional
+    public Recipe createRecipe(Recipe recipe, String username) {
         Member currentUser = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
 
-        // 레시피와 사용자를 연결
+        if (recipe.getRecipeName() == null || recipe.getRecipeName().trim().isEmpty()) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_RECIPE_NAME_FORMAT);
+        }
+        if (recipe.getRecipeDescription() == null || recipe.getRecipeDescription().trim().isEmpty()) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_RECIPE_DESCRIPTION_FORMAT);
+        }
+
         recipe.setMember(currentUser);
-
-        // DB에 레시피 저장
         return recipeRepository.save(recipe);
     }
 
-    public Recipe updateRecipe(Recipe recipe) {
-        return recipeRepository.save(recipe);
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public Recipe updateRecipe(Recipe newRecipeData, String currentPrincipalName) {
+        if (newRecipeData.getRecipeName() == null || newRecipeData.getRecipeName().trim().isEmpty()) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_RECIPE_NAME_FORMAT);
+        }
+        if (newRecipeData.getRecipeDescription() == null || newRecipeData.getRecipeDescription().trim().isEmpty()) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_RECIPE_DESCRIPTION_FORMAT);
+        }
+
+        Recipe currentRecipe = recipeRepository.findById(newRecipeData.getRecipeId())
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.RECIPE_NOT_FOUND));
+
+        Member currentUser = memberRepository.findByUsername(currentPrincipalName)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        if (!currentUser.getUsername().equals(currentRecipe.getMember().getUsername())) {
+            throw new BusinessLogicException(ExceptionCode.UNAUTHORIZED_ACCESS);
+        }
+
+        // 레시피 수정 로직
+        currentRecipe.setRecipeName(newRecipeData.getRecipeName());
+        currentRecipe.setCookTime(newRecipeData.getCookTime());
+        currentRecipe.setRecipeDescription(newRecipeData.getRecipeDescription());
+
+        Optional.ofNullable(newRecipeData.getFoodType())
+                .ifPresent(foodType -> currentRecipe.setFoodType(foodType));
+        Optional.ofNullable(newRecipeData.getMainImageUrl())
+                .ifPresent(mainImageUrl -> currentRecipe.setMainImageUrl(mainImageUrl));
+        Optional.ofNullable(newRecipeData.getDifficulty())
+                .ifPresent(difficulty -> currentRecipe.setDifficulty(difficulty));
+        Optional.ofNullable(newRecipeData.getRecipeStep())
+                .ifPresent(steps -> currentRecipe.setRecipeStep(steps));
+        Optional.ofNullable(newRecipeData.getIngredients())
+                .ifPresent(ingredients -> currentRecipe.setIngredients(ingredients));
+
+        return recipeRepository.save(currentRecipe);
     }
 
+    @Transactional(readOnly = true)
     public Recipe findRecipeById(Long recipeId) {
         return recipeRepository.findById(recipeId)
                 .orElseThrow(() -> new NoSuchElementException("Recipe not found"));
     }
 
+    @Transactional(readOnly = true)
     public List<Recipe> findAllRecipes(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return recipeRepository.findAll(pageable).getContent();
     }
 
+    @Transactional
     public void deleteRecipeById(Long recipeId) {
         recipeRepository.deleteById(recipeId);
     }
+
+//    public List<RecipeDto.RelatedRecipeDto> findRelatedRecipes(Long recipeId, int offset) {
+//    }
 }
