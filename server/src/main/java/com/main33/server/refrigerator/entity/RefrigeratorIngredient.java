56fa0bfbb3a78f1a6cd011edce0e12912978922a
spirit998 <spirit998@naver.com>
2023-09-18 14:19:44 +0900

    Refactor: 메서드 이름 수정

--- server/src/main/java/com/main33/server/refrigerator/entity/RefrigeratorIngredient.java
+++ server/src/main/java/com/main33/server/refrigerator/entity/RefrigeratorIngredient.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @NoArgsConstructor
-public class RefrigeratorIngredient {
+public class    RefrigeratorIngredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
